44fc074dd256ca3d8b67d9b5cc51e113af5282bf
hheelee <hheelee@smilegate.com>
2020-09-13 01:57:25 +0900

    Initial commit

---
+++ src/main/java/com/example/kakaopay/sprinkle_pay/SprinklePayApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaopay.sprinkle_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinklePayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinklePayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kakaopay/sprinkle_pay/SprinklePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaopay.sprinkle_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinklePayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
